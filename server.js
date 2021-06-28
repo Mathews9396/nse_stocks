e5b3b4aafba23a406813d05a717135f65f1633ee
Mathews Mathews <mathewsmathews95@gmail.com>
2021-06-28 16:28:00 +0530

    prep for deployment_14

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ app.use(express.json());
 app.use(cors());
 // app.use(express.static('build'));
 
-// const whitelist = ['http://localhost:3000', 'http://localhost:8080']
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     console.log("** Origin of request " + origin)
-//     if (whitelist.indexOf(origin) !== -1 || !origin) {
-//       console.log("Origin acceptable")
-//       callback(null, true)
-//     } else {
-//       console.log("Origin rejected")
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
-// app.use(cors(corsOptions))
+const whitelist = ['http://localhost:3000', 'http://localhost:8080']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 const PORT = process.env.PORT || 3001;
 
